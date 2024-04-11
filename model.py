a6e7deeca6d34188950568b6f3cfad2e7b75fa01
DamFil <damjan.filipovic.21@um.edu.mt>
2024-04-11 17:35:20 +0200

    Better train-test split with equal distributions across classes

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torch.nn.functional as fun
 from torch.utils.data import Dataset, DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms import ToTensor
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, train_test_split
 import pandas as pd
 import numpy as np
 import nibabel as nib
@@ -18,8 +18,6 @@ labels = {
     1: "AD",
     2: "MCI",
 }
-
-#TODO: Create separate training, validation and testing datasets
 
 ############################## DATASET DEFINITION ##############################
 class MriDataset(Dataset):
@@ -48,9 +46,21 @@ class MriDataset(Dataset):
             label = self.target_transform(label)
         return mri, label
 
+    def get_labels(self):
+        labels = np.array([])
+        for i in range(len(self.img_labels)):
+            label = self.img_labels.iloc[i,1]
+            labels = np.append(labels, label)
+
+        return labels
+
 datasetdir = "/home/damfil/Uni/FYP/resources/mri/ad/dataset/NiftiDataset/RegShortDataset/"
 full_dataset = MriDataset(datasetdir+"labels.csv", datasetdir, ToTensor(), None)
-train_dataset, test_dataset = random_split(full_dataset, [0.8, 0.2])
+
+labels = full_dataset.get_labels()
+train_idx, test_idx = train_test_split(np.arange(len(labels)), test_size=0.2, shuffle=True, stratify=labels)
+train_dataset = torch.utils.data.Subset(full_dataset, train_idx)
+test_dataset = torch.utils.data.Subset(full_dataset, test_idx)
 
 ############################## MODEL DEFINITION ##############################
 device = (
@@ -133,20 +143,8 @@ class ADNet(nn.Module):
 
         # fully connected layers
         x = x.view(x.size(0),-1)
-
         x = fun.dropout(self.bn6(fun.relu(self.fc1(x))))
-        # x = self.fc1(x)
-        # x = fun.relu(x)
-        # x = self.bn6(x)
-        # x = fun.dropout(x)
-
         x = fun.dropout(self.bn6(fun.relu(self.fc2(x))))
-
-        # x = self.fc2(x)
-        # x = fun.relu(x)
-        # x = self.bn6(x)
-        # x = fun.dropout(x)
-
         x = fun.softmax(self.bn7(self.fc3(x)))
 
         return x
@@ -160,13 +158,54 @@ batch_size = 4
 train_losses = np.array([]); val_losses = np.array([])
 train_accs = np.array([]); val_accs = np.array([])
 
+params_pre_0 = []
+params_post_0 = []
+params_pre_1 = []
+params_post_1 = []
+
+adnet = ADNet().to(device)
+opt_fn = torch.optim.Adam(adnet.parameters(), 2e-3)
+loss_fn = nn.CrossEntropyLoss()
+
 kf = KFold(num_folds, shuffle=True)
 timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-loss_fn = nn.CrossEntropyLoss()
 writer = SummaryWriter("/home/damfil/Uni/FYP/PyTorchADNet/sample_logs")
+
+
+def save_params(parameters, arr):
+    for param in adnet.parameters():
+        arr.append(param)
+
+def check_params():
+    if len(params_pre_0) != len(params_post_0):
+        print(f"Number of weights does not stay the same before and after epoch 0")
+    
+    if len(params_post_0) != len(params_pre_1):
+        print(f"Number of weights changes between the end of epoch 0 and start of epoch 1")
+        return
+
+    for i in range(len(params_pre_1)):
+        if not torch.equal(params_pre_1[i], params_post_0[i]):
+            print(f"The weights are not same after epoch 0 finishes and epoch 1 starts")
+            break
+        
+    return
+
+def get_length(parameters):
+    counter = 0
+    for param in parameters:
+        counter += 1
+
+    print(f"Mum. of parameters: [{counter}]")
+    return counter
+
 
 def train_one_epoch(model, dataloader, epoch_idx, sum_writer, optimizer, loss_fn):
     model.train(True)
+    if epoch_idx == 0:
+        save_params(model.parameters(), params_pre_0)
+    elif epoch_idx == 1:
+        save_params(model.parameters(), params_pre_1)
 
     running_loss = 0.
     num_correct = 0
@@ -196,7 +235,6 @@ def train_one_epoch(model, dataloader, epoch_idx, sum_writer, optimizer, loss_fn
     accuracy = 100 * num_correct / len(dataloader.dataset)
 
     sum_writer.add_scalar('Loss/train', avg_loss, epoch_idx)
-
     print(f"[TRAIN]: Epoch [{epoch_idx}] - Avg. loss per batch: [{avg_loss}] - Accuracy: [{accuracy}%]")
 
     return avg_loss, accuracy
@@ -241,14 +279,16 @@ def train_model():
         trainloader = DataLoader(train_dataset, batch_size, sampler=torch.utils.data.SubsetRandomSampler(train_idx))
         valloader = DataLoader(train_dataset, batch_size, sampler=torch.utils.data.SubsetRandomSampler(val_idx))
 
-        adnet = ADNet().to(device)
-        opt_fn = torch.optim.Adam(adnet.parameters(), 2e-3)
-
         v_loss = 0
         for i in range(num_epochs):
 
             # train the model
             t_loss, t_acc  = train_one_epoch(adnet, trainloader, i, writer, opt_fn, loss_fn)
+            if i == 0:
+               save_params(adnet.parameters(), params_post_0)
+            elif i == 1:
+               save_params(adnet.parameters(), params_post_1)
+               check_params()
 
             # validate the model on the parameters
             v_loss, v_acc = validate_one_epoch(adnet, valloader, i, writer)
