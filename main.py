f8249d23b5eb17342b876a2830e99f9af6d98818
DamFil <damjan.filipovic.21@um.edu.mt>
2024-04-23 13:06:40 +0200

    Printing train and test dataset sizes

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import sys
 import numpy as np
-import torch
 from torch import nn, optim
 from torch.utils.data import Subset, DataLoader
 from torchvision.transforms import ToTensor
 from sklearn.model_selection import train_test_split
-from tqdm import tqdm
 
 from Utils import parse_args
 from DatasetHandler import MriDataset
-from Model import ADNet, LeNet3D, device
+from Model import ADNet, device
 from TrainingAlgorithm import train_model, test_model
 
 # labels map
@@ -26,6 +24,13 @@ def main(arguments):
 
     labels = full_dataset.get_labels()
     train_idx, test_idx = train_test_split(np.arange(len(labels)), test_size=0.2, shuffle=True, stratify=labels)
+
+    print(" CONFIRMING TESTING AND TRAINING SPLITS ")
+    print("----------------------------------------")
+    print(f" TRAIN SET LENGTH: [{len(train_idx)}]")
+    print(f" TEST SET LENGTH: [{len(test_idx)}]")
+    print("----------------------------------------")
+
     train_dataset = Subset(full_dataset, train_idx)
     test_dataset = Subset(full_dataset, test_idx)
     test_labels = full_dataset.get_labels(test_idx)
@@ -36,14 +41,14 @@ def main(arguments):
 
     adam = optim.Adam(adnet.parameters(), 0.0001)
     # sgd = optim.SGD(adnet.parameters(), lr=0.0001, momentum=0.9)
-    # adam2 = optim.Adam(lenet.parameters(), 0.0001)
+    # adam = optim.Adam(lenet.parameters(), 0.0001)
     # sgd2 = optim.SGD(lenet.parameters(), lr=0.0001, momentum=0.9)
 
     cross_entropy = nn.CrossEntropyLoss()
     train_labels = full_dataset.get_labels(train_idx)
     num_epochs = args.epochs
-    num_folds = 5
-    batch_size = 4
+    num_folds = 4
+    batch_size = 3
 
     # training
     train_model(adnet, adam, cross_entropy, train_dataset, train_labels, batch_size, num_epochs, num_folds, device)
