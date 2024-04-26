e13940cb6fd7eb77d5aa60280fcbfa69a302c2b7
DamFil <damjan.filipovic.21@um.edu.mt>
2024-04-26 14:41:44 +0200

    Improved the get_labels method by making use of library features better also the first row of the csv file does not get ignored anymore

--- DatasetHandler.py
+++ DatasetHandler.py
@@ -4,9 +4,10 @@ import nibabel as nib
 import pandas as pd
 import os
 
+
 class MriDataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
+        self.img_labels = pd.read_csv(annotations_file, header=None)
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
@@ -18,7 +19,7 @@ class MriDataset(Dataset):
         mri_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
         # convert from Nifti1 to numpy - this can be done as part of the transform
         mri = nib.load(mri_path)
-        mri = np.array(mri.dataobj,dtype=np.ubyte)
+        mri = np.array(mri.dataobj, dtype=np.ubyte)
         # remove the 4th dimension
         if mri.ndim == 4:
             mri = mri[:, :, :, 0]
@@ -32,15 +33,8 @@ class MriDataset(Dataset):
         return mri, label
 
     def get_labels(self, indices=None):
-        labels = np.array([])
-
         if indices is None:
-            for i in range(len(self.img_labels)):
-                label = self.img_labels.iloc[i,1]
-                labels = np.append(labels, label)
-        else:
-            for idx in np.ndenumerate(indices):
-                label = self.img_labels.iloc[idx[1],1]
-                labels = np.append(labels, label)
+            return self.img_labels.iloc[:, 1].to_numpy()
 
-        return labels
+        return self.img_labels.iloc[indices, 1].to_numpy()
+
