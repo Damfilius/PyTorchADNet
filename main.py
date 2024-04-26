668115c0186ebe7e00f25d623ab2f99cd36be5e7
DamFil <damjan.filipovic.21@um.edu.mt>
2024-04-26 14:43:14 +0200

    Added a line to make use of the 2nd training function with fixed valid

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from torch.utils.data import Subset, DataLoader
 from torchvision.transforms import ToTensor
 from sklearn.model_selection import train_test_split
 
-from Utils import parse_args
+from Utils import parse_args, calculate_distribution
 from DatasetHandler import MriDataset
-from Model import ADNet, device
-from TrainingAlgorithm import train_model, test_model
+from Model import ADNet, device, LeNet3D
+from TrainingAlgorithm import train_model, train_model_2, test_model
 
 # labels map
 
@@ -25,10 +25,18 @@ def main(arguments):
     labels = full_dataset.get_labels()
     train_idx, test_idx = train_test_split(np.arange(len(labels)), test_size=0.2, shuffle=True, stratify=labels)
 
+    tr_dist = calculate_distribution(labels[train_idx], [0, 1, 2])
+    te_dist = calculate_distribution(labels[test_idx], [0, 1, 2])
+
+    if np.intersect1d(train_idx, test_idx).size == 0:
+        print("No overlap between training and testing sets...\n")
+
     print(" CONFIRMING TESTING AND TRAINING SPLITS ")
     print("----------------------------------------")
     print(f" TRAIN SET LENGTH: [{len(train_idx)}]")
+    print(f" TRAIN DISTRIBUTION: {tr_dist}")
     print(f" TEST SET LENGTH: [{len(test_idx)}]")
+    print(f" TEST DISTRIBUTION: {te_dist}")
     print("----------------------------------------")
 
     train_dataset = Subset(full_dataset, train_idx)
@@ -36,27 +44,29 @@ def main(arguments):
     test_labels = full_dataset.get_labels(test_idx)
 
     # model
-    adnet = ADNet().to(device)
-    # lenet = LeNet3D().to(device)
+    # adnet = ADNet().to(device)
+    lenet = LeNet3D().to(device)
 
-    adam = optim.Adam(adnet.parameters(), 0.0001)
+    # adam = optim.Adam(adnet.parameters(), 0.0001)
     # sgd = optim.SGD(adnet.parameters(), lr=0.0001, momentum=0.9)
-    # adam = optim.Adam(lenet.parameters(), 0.0001)
+    adam = optim.Adam(lenet.parameters(), 0.0001)
     # sgd2 = optim.SGD(lenet.parameters(), lr=0.0001, momentum=0.9)
 
     cross_entropy = nn.CrossEntropyLoss()
     train_labels = full_dataset.get_labels(train_idx)
     num_epochs = args.epochs
-    num_folds = 4
-    batch_size = 3
+    num_folds = 5
+    batch_size = 1
 
     # training
-    train_model(adnet, adam, cross_entropy, train_dataset, train_labels, batch_size, num_epochs, num_folds, device)
+    train_model(lenet, adam, cross_entropy, train_dataset, train_labels, batch_size, num_epochs, num_folds, device)
+    # train_model_2(lenet, adam, cross_entropy, train_dataset, train_labels, batch_size, num_epochs, device)
+
     print("FINISHED TRAINING - STARTED TESTING")
     # lenet.load_state_dict(torch.load("model_20240421_180514_4"))
     # print("Successfully loaded the model...")
     # testing
-    avg_loss, conf_mat, f1_scores = test_model(adnet, cross_entropy, test_dataset, test_labels, batch_size, device)
+    avg_loss, conf_mat, f1_scores = test_model(lenet, cross_entropy, test_dataset, test_labels, batch_size, device)
     print("FINISHED TESTING")
 
     print("CONFUSION MATRIX")
