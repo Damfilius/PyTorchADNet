c2983871380f107ba8641d6b6b6ed3b1f45390f1
DamFil <damjan.filipovic.21@um.edu.mt>
2024-04-29 10:19:21 +0200

    Cleaned up AdNet definition

--- Model.py
+++ Model.py
@@ -13,18 +13,9 @@ device = (
 print(f"Using {device} device")
 
 
-# defining the architecture of the model
-# model expects an input of size (4, 1, 91, 91, 109)
 class ADNet(nn.Module):
     def __init__(self):
         super(ADNet, self).__init__()
-
-        self.n_filters = {'l1': 32,
-                          'l2': 64,
-                          'l3': 128,
-                          'l4': 256,
-                          'l5': 256,
-                          'fc': 512}
 
         self.filter_size = (3, 3, 3)
         self.padding = (1, 1, 1)
@@ -34,65 +25,64 @@ class ADNet(nn.Module):
 
         #---------------------------- CONVOLUTION AND BN LAYERS ----------------------------
 
-        # layer 1
-        self.conv1 = nn.Conv3d(1, self.n_filters['l1'], self.filter_size, self.stride, self.padding)
-        self.bn1 = nn.BatchNorm3d(self.n_filters['l1'])
-        self.pool1 = nn.MaxPool3d(self.pool_size, self.pool_stride, (1, 0, 1))
-
-        # layer 2
-        self.conv2 = nn.Conv3d(self.n_filters['l1'], self.n_filters['l2'], self.filter_size, self.stride, self.padding)
-        self.bn2 = nn.BatchNorm3d(self.n_filters['l2'])
-        self.pool2 = nn.MaxPool3d(self.pool_size, self.pool_stride, (1, 1, 0))
-
-        # layer 3
-        self.conv3a = nn.Conv3d(self.n_filters['l2'], self.n_filters['l3'], self.filter_size, self.stride, self.padding)
-        self.conv3b = nn.Conv3d(self.n_filters['l3'], self.n_filters['l3'], self.filter_size, self.stride, self.padding)
-        self.bn3 = nn.BatchNorm3d(self.n_filters['l3'])
-        self.pool3 = nn.MaxPool3d(self.pool_size, self.pool_stride, (1, 0, 1))
-
-        # layer 4
-        self.conv4a = nn.Conv3d(self.n_filters['l3'], self.n_filters['l4'], self.filter_size, self.stride, self.padding)
-        self.conv4b = nn.Conv3d(self.n_filters['l4'], self.n_filters['l4'], self.filter_size, self.stride, self.padding)
-        self.bn4 = nn.BatchNorm3d(self.n_filters['l4'])
-        self.pool4 = nn.MaxPool3d(self.pool_size, self.pool_stride, (1, 1, 0))
-
-        # layer 5
-        self.conv5a = nn.Conv3d(self.n_filters['l4'], self.n_filters['l5'], self.filter_size, self.stride, self.padding)
-        self.conv5b = nn.Conv3d(self.n_filters['l5'], self.n_filters['l5'], self.filter_size, self.stride, self.padding)
-        self.bn5 = nn.BatchNorm3d(self.n_filters['l5'])
-        self.pool5 = nn.MaxPool3d(self.pool_size, self.pool_stride)
+        self.convb1 = self.conv_block(1, 32, (1, 0, 1))
+        self.convb2 = self.conv_block(32, 64, (1, 1, 0))
+        self.convb3 = self.double_conv_block(64, 128, (1, 0, 1))
+        self.convb4 = self.double_conv_block(128, 256, (1, 1, 0))
+        self.convb5 = self.double_conv_block(256, 256, 0)
 
         #---------------------------- FULLY CONNECTED LAYERS ----------------------------
 
-        # layer 6 and 7
-        self.fc1 = nn.Linear(256 * 6 * 6 * 6, self.n_filters['fc'])
-        self.fc2 = nn.Linear(self.n_filters['fc'], self.n_filters['fc'])
-        self.bn6 = nn.BatchNorm1d(self.n_filters['fc'])
-
-        #---------------------------- OUTPUT LAYERS ----------------------------
-
-        # layer 8
-        self.fc3 = nn.Linear(self.n_filters['fc'], 3)
-        self.bn7 = nn.BatchNorm1d(3)
-
-        #---------------------------- WEIGHT INIT ----------------------------
-        self.init_weights()
+        self.fc1 = self.fc_block(256 * 6 * 6 * 6, 512)
+        self.fc2 = self.fc_block(256 * 6 * 6 * 6, 512)
+        self.fc3 = self.output_layer(512, 3)
 
     def forward(self, x):
         # convolutions
-        x = self.pool1(self.bn1(fun.relu(self.conv1(x))))
-        x = self.pool2(self.bn2(fun.relu(self.conv2(x))))
-        x = self.pool3(self.bn3(fun.relu(self.conv3b(self.bn3(fun.relu(self.conv3a(x)))))))
-        x = self.pool4(self.bn4(fun.relu(self.conv4b(self.bn4(fun.relu(self.conv4a(x)))))))
-        x = self.pool5(self.bn5(fun.relu(self.conv5b(self.bn5(fun.relu(self.conv5a(x)))))))
-
-        # fully connected layers
+        x = self.convb1(x)
+        x = self.convb2(x)
+        x = self.convb3(x)
+        x = self.convb4(x)
+        x = self.convb5(x)
         x = x.view(-1, 256 * 6 * 6 * 6)
-        x = fun.dropout(self.bn6(fun.relu(self.fc1(x))))
-        x = fun.dropout(self.bn6(fun.relu(self.fc2(x))))
-        x = fun.softmax(self.bn7(self.fc3(x)))
-
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
         return x
+
+    def conv_block(self, in_channels, out_channels, pool_pad):
+        return nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, self.filter_size, self.stride, self.padding),
+            nn.ReLU(),
+            nn.BatchNorm3d(out_channels),
+            nn.MaxPool3d(self.pool_size, self.pool_stride, pool_pad)
+        )
+
+    def double_conv_block(self, in_channels, out_channels, pool_pad):
+        return nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, self.filter_size, self.stride, self.padding),
+            nn.ReLU(),
+            nn.BatchNorm3d(out_channels),
+            nn.Conv3d(out_channels, out_channels, self.filter_size, self.stride, self.padding),
+            nn.ReLU(),
+            nn.BatchNorm3d(out_channels),
+            nn.MaxPool3d(self.pool_size, self.pool_stride, pool_pad)
+        )
+
+    def fc_block(self, in_features, out_features):
+        return nn.Sequential(
+            nn.Linear(in_features, out_features),
+            nn.ReLU(),
+            nn.BatchNorm1d(out_features),
+            nn.Dropout1d()
+        )
+
+    def output_layer(self, in_features, out_features):
+        return nn.Sequential(
+            nn.Linear(in_features, out_features),
+            nn.BatchNorm1d(out_features),
+            nn.Softmax()
+        )
 
     def init_weights(self):
         for param in self.parameters():
