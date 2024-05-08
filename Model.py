842393cc834446389dfc5635333b549ba2f7fcd8
DamFil <damjan.filipovic.21@um.edu.mt>
2024-05-08 16:18:13 +0200

    Passing right parameters to conv3d layers in ADNet

--- Model.py
+++ Model.py
@@ -27,40 +27,40 @@ class ADNet(nn.Module):
 
         self.features = nn.Sequential(
             # conv1
-            nn.Conv3d(1, 32, (3, 3, 3), padding=1),
+            nn.Conv3d(1, 32, kernel_size=self.filter_size, stride=self.stride, padding=self.padding),
             nn.BatchNorm3d(32),
             nn.ReLU(),
             nn.MaxPool3d(2, stride=2, return_indices=True, padding=(1, 0, 1)),
 
             # conv2
-            nn.Conv3d(32, 64, 3, padding=1),
+            nn.Conv3d(32, 64, kernel_size=self.filter_size, stride=self.stride, padding=self.padding),
             nn.BatchNorm3d(64),
             nn.ReLU(),
             nn.MaxPool3d(2, stride=2, return_indices=True, padding=(1, 1, 0)),
 
             # conv3
-            nn.Conv3d(64, 128, 3, padding=1),
+            nn.Conv3d(64, 128, kernel_size=self.filter_size, stride=self.stride, padding=self.padding),
             nn.BatchNorm3d(128),
             nn.ReLU(),
-            nn.Conv3d(128, 128, padding=(1, 0, 1)),
+            nn.Conv3d(128, 128, kernel_size=self.filter_size, stride=self.stride, padding=self.padding),
             nn.BatchNorm3d(128),
             nn.ReLU(),
             nn.MaxPool3d(2, stride=2, return_indices=True, padding=(1, 0, 1)),
 
             # conv4
-            nn.Conv3d(128, 256, 3, padding=1),
+            nn.Conv3d(128, 256, kernel_size=self.filter_size, stride=self.stride, padding=self.padding),
             nn.BatchNorm3d(256),
             nn.ReLU(),
-            nn.Conv3d(256, 256, 3, padding=1),
+            nn.Conv3d(256, 256, kernel_size=self.filter_size, stride=self.stride, padding=self.padding),
             nn.BatchNorm3d(256),
             nn.ReLU(),
             nn.MaxPool3d(2, stride=2, return_indices=True, padding=(1, 1, 0)),
 
             # conv5
-            nn.Conv3d(256, 256, 3, padding=1),
+            nn.Conv3d(256, 256, kernel_size=self.filter_size, stride=self.stride, padding=self.padding),
             nn.BatchNorm3d(256),
             nn.ReLU(),
-            nn.Conv3d(256, 256, 3, padding=1),
+            nn.Conv3d(256, 256, kernel_size=self.filter_size, stride=self.stride, padding=self.padding),
             nn.BatchNorm3d(256),
             nn.ReLU(),
             nn.MaxPool3d(2, stride=2, return_indices=True, padding=0),
@@ -113,3 +113,62 @@ class LeNet3D(nn.Module):
         x = fun.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+class ResidualBlock(nn.Module):
+    def __init__(self, inchannel, outchannel, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.left = nn.Sequential(
+            nn.Conv3d(inchannel, outchannel, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm3d(outchannel),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(outchannel, outchannel, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm3d(outchannel)
+        )
+        self.shortcut = nn.Sequential()
+        if stride != 1 or inchannel != outchannel:
+            self.shortcut = nn.Sequential(
+                nn.Conv3d(inchannel, outchannel, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm3d(outchannel)
+            )
+
+    def forward(self, x):
+        out = self.left(x)
+        out = out + self.shortcut(x)
+        out = fun.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, ResidualBlock, num_classes=3):
+        super(ResNet, self).__init__()
+        self.inchannel = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv3d(1, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm3d(64),
+            nn.ReLU()
+        )
+        self.layer1 = self.make_layer(ResidualBlock, 64, 2, stride=1)
+        self.layer2 = self.make_layer(ResidualBlock, 128, 2, stride=2)
+        self.layer3 = self.make_layer(ResidualBlock, 256, 2, stride=2)
+        self.layer4 = self.make_layer(ResidualBlock, 512, 2, stride=2)
+        self.fc = nn.Linear(512, num_classes)
+
+    def make_layer(self, block, channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inchannel, channels, stride))
+            self.inchannel = channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = fun.avg_pool3d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
