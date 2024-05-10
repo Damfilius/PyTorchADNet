4cc5318e74c477b9ca504aac6a10625d243f1a4b
DamFil <damjan.filipovic.21@um.edu.mt>
2024-05-10 17:20:12 +0200

    Added LeNet with batch normalization

--- Model.py
+++ Model.py
@@ -114,6 +114,28 @@ class LeNet3D(nn.Module):
         x = self.fc3(x)
         return x
 
+class LeNet3DBn(nn.Module):
+    def __init__(self):
+        super(LeNet3D, self).__init__()
+
+        self.conv1 = nn.Conv3d(1, 6, kernel_size=(5, 5, 5))
+        self.bn1 = nn.BatchNorm3d(6)
+        self.pool = nn.MaxPool3d(2, 2)
+        self.conv2 = nn.Conv3d(6, 16, kernel_size=(5, 5, 5))
+        self.bn2 = nn.BatchNorm3d(16)
+        self.fc1 = nn.Linear(16 * 44 * 36 * 36, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 3)
+
+    def forward(self, x):
+        x = self.pool(fun.relu(self.bn1(self.conv1(x))))
+        x = self.pool(fun.relu(self.bn2(self.conv2(x))))
+        x = x.view(-1, 16 * 44 * 36 * 36)
+        x = fun.relu(self.fc1(x))
+        x = fun.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
 class ResidualBlock(nn.Module):
     def __init__(self, inchannel, outchannel, stride=1):
         super(ResidualBlock, self).__init__()
